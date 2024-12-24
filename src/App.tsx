2b035029ccf2a73ecec081014fe35a3c92129bb0
Eugene Lu <eugenelu9848@gmail.com>
2024-12-23 21:57:01 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import Launch from "./pages/Launch/Launch";
+
+function App() {
+  return <Launch/>
+}
+
+export default App
---
+++ src/pages/Launch/Launch.tsx
@@ -0,0 +1,10 @@
+import './Launch.css';
+
+function Launch() {
+  return (
+    <div className="launch-page">
+    </div>
+  );
+};
+
+export default Launch;
